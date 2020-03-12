a625a4221006e05180bb4bc004062df7d0a9d77c
xietianfu <826597263@qq.com>
2020-03-12 23:41:20 +0800

    feat: 获取满足条件的文件

--- constants/paths.js
+++ constants/paths.js
@@ -11,8 +11,39 @@ function getConfigPath(name) {
   return dtaConfigData[name];
 }
 
+/**
+ * @returns {Array}  按创建时间由近到远排列
+ */
 function getMeetApiFiles() {
-  const { downPath, name } = dtaConfigData;
+  const runPath = shell.pwd().stdout;
+  let apiFiles = [];
+  if (dtaConfigData) {
+    const { downPath, name } = dtaConfigData;
+    shell.cd(downPath);
+    let results = shell.ls("-l", `./${name}*.json`);
+    if (!results.stderr) {
+      // note: 排序直接在results上排序无法生效,提示stderr,原因未知,待确认
+      apiFiles = results.map(item => ({
+        name: item.name,
+        birthtimeMs: item.birthtimeMs
+      }));
+      apiFiles.sort((a, b) => {
+        if (a.birthtimeMs > b.birthtimeMs) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      apiFiles = apiFiles.map(item => item.name);
+    } else {
+      console.log(results.stderr);
+    }
+  } else {
+    console.log("err");
+  }
+
+  shell.cd(runPath);
+  return apiFiles;
 }
 
 const apiPath = getConfigPath("downPath");
@@ -21,8 +52,8 @@ const constants = resolveApp("constants");
 const src = resolveApp("src");
 const runPath = shell.pwd().stdout;
 const globalConfig = resolveApp("globalConfig.json");
-const globalDownPath = fs.readJSONSync(resolveApp("globalConfig.json"))
-  .downPath;
+const globalDownPath = fs.readJSONSync(globalConfig).downPath;
+const meetApiFiles = getMeetApiFiles();
 
 module.exports = {
   root: appDirectory,
@@ -32,5 +63,6 @@ module.exports = {
   globalConfig,
   globalDownPath,
   apiPath,
-  outPath
+  outPath,
+  meetApiFiles
 };
---
+++ src/verifyConfig.js
@@ -0,0 +1,2 @@
+const files = require("../constants/files");
+const paths = require("../constants/paths");
