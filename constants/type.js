1d3d27f2c160a293012f7411b9d6a19bdf03494b
xietianfu <hello-world2018@outlook.com>
2021-12-02 15:52:43 +0800

    feat: 函数处理

--- constants/type.js
+++ constants/type.js
@@ -4,7 +4,7 @@ const typeList = {
   2: "Boolean",
   3: "Object[]",
   4: "Object",
-  5: "Mixed"
+  5: "Mixed",
 };
 
 const innerTypeList = {
@@ -13,20 +13,20 @@ const innerTypeList = {
   2: "Boolean",
   3: "Array",
   4: "Object",
-  5: "Mixed"
+  5: "Mixed",
 };
 
 // 请求参数是否必传
 const mustType = {
   1: "Y",
-  0: "N"
+  0: "N",
 };
 
-const configName = "dtaConfig.json";
+const configName = "apiConfig.json";
 
 module.exports = {
   typeList,
   configName,
   innerTypeList,
-  mustType
+  mustType,
 };
--- src/index.js
+++ src/index.js
@@ -19,28 +19,28 @@ shell.config.silent = true;
 
 program.version("1.0.4");
 
-program
-  .command("init")
-  .description("生成基础配置文件")
-  .action(() => {
-    initConfig();
-  });
+// program
+//   .command("init")
+//   .description("生成基础配置文件")
+//   .action(() => {
+//     initConfig();
+//   });
 
-program
-  .command("setDir")
-  .description("设置读取接口文件所在的目录")
-  .action(path => {
-    setDownPath();
-  });
+// program
+//   .command("setDir")
+//   .description("设置读取接口文件所在的目录")
+//   .action(path => {
+//     setDownPath();
+//   });
 
-program
-  .command("setBen")
-  .description("为新下载的接口设置一个参考文件,用以获取接口变化")
-  .action(path => {
-    if (!isEmptyParam(files.dtaConfigData, "未找到相关api生成配置")) {
-      setBenchmark().then(res => console.log(chalk.green("参考文件设置成功")));
-    }
-  });
+// program
+//   .command("setBen")
+//   .description("为新下载的接口设置一个参考文件,用以获取接口变化")
+//   .action(path => {
+//     if (!isEmptyParam(files.dtaConfigData, "未找到相关api生成配置")) {
+//       setBenchmark().then(res => console.log(chalk.green("参考文件设置成功")));
+//     }
+//   });
 
 program
   .command("start")
@@ -58,37 +58,37 @@ program
         outPath: paths.outPath,
         outName: files.dtaConfigData.outName,
         axiosPath: files.dtaConfigData.axiosFile,
-        cutOff: files.dtaConfigData.cutOff
+        cutOff: files.dtaConfigData.cutOff,
       });
       console.log(chalk.green("接口函数生成成功"));
     }
   });
 
-program
-  .command("view")
-  .description("查看本次更新接口")
-  .action(() => {
-    const result = compare({
-      benchmarkApi: fs.readJSONSync(
-        path.resolve(paths.apiPath, files.dtaConfigData.benchmark)
-      ),
-      compareApi: fs.readJSONSync(
-        path.resolve(paths.apiPath, paths.meetApiFiles[0])
-      )
-    });
-    madeChangeMd({
-      changeData: result,
-      benchmarkName: paths.meetApiFiles[1],
-      compareName: paths.meetApiFiles[0],
-      outPath: paths.outPath
-    });
-    console.log(chalk.green("接口变更文档生成成功"));
-  });
+// program
+//   .command("view")
+//   .description("查看本次更新接口")
+//   .action(() => {
+//     const result = compare({
+//       benchmarkApi: fs.readJSONSync(
+//         path.resolve(paths.apiPath, files.dtaConfigData.benchmark),
+//       ),
+//       compareApi: fs.readJSONSync(
+//         path.resolve(paths.apiPath, paths.meetApiFiles[0]),
+//       ),
+//     });
+//     madeChangeMd({
+//       changeData: result,
+//       benchmarkName: paths.meetApiFiles[1],
+//       compareName: paths.meetApiFiles[0],
+//       outPath: paths.outPath,
+//     });
+//     console.log(chalk.green("接口变更文档生成成功"));
+//   });
 
 program
   .command("config <file>")
   .description("查看文件的各种配置")
-  .action(file => {
+  .action((file) => {
     console.log({ files, paths, type, index }[file]);
   });
 
